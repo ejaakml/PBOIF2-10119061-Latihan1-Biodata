881d8b600768fc96f3d8f0635d9ed24ccb2d6536
ejaakml <eximindotong@yahoo.com>
2020-10-20 16:56:10 +0700

    p

---
+++ src/pboif2/pkg10119061/latihan1/biodata/PBOIF210119061Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan1.biodata;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------------");
+        System.out.println("NAMA\t\t: Rheiza Akmal Razaky");
+        System.out.println("ALAMAT\t\t: Serang Banten");
+        System.out.println("TEMPAT LAHIR\t: Serang");
+        System.out.println("TANGGAL LAHIR\t:13 JANUARI 2001");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t:Motoran");
+        System.out.println("MAKES\t\t: Ayam");
+        System.out.println("MIKES\t\t: Kopi");
+    }
+    
+}
